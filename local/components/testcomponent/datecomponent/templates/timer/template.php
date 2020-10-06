cb390072b1f42c1880af40127752a224a8a91f98
fenibazil <feni9214273281@gmail.com>
2020-10-06 18:17:10 +0300

    create test component

---
+++ local/components/testcomponent/datecomponent/templates/timer/template.php
@@ -0,0 +1,6 @@
+<? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+      echo $arResult['DATE'];
+
+      echo "<input type="time" name="time" value="" disabled>
+            <input type="button" name="time_btn">";
+?>
