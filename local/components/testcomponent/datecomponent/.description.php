360a8071c21244f398b88ca2fee4079ced396bc2
fenibazil <feni9214273281@gmail.com>
2020-10-06 15:37:41 +0300

    create test component

--- local/components/testcomponent/datecomponent/.description.php
+++ local/components/testcomponent/datecomponent/.description.php
@@ -1,7 +1,7 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die(); 
 $arComponentDescription = array(
-"NAME" => GetMessage("Текущая дата"),
-"DESCRIPTION" => GetMessage("Выводим текущую дату"),
+"NAME" => GetMessage("TC_Curdate"),
+"DESCRIPTION" => GetMessage("TC_Curdate_Show"),
 "PATH" => array(
     "ID" => "test_data_component",
     "NAME" => GetMessage("TestDataComponent"),
---
+++ local/components/testcomponent/datecomponent/lang/ru/.description.php
@@ -0,0 +1,5 @@
+<?
+$MESS ['TC_Curdate'] = "текушая дата";
+$MESS ['TC_Curdate_Show'] = "Выводим текущую дату";
+$MESS ['TestDataComponent'] = "Тестовый компонент даты";
+?>
